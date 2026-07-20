b4ce633ce18543d94decda2de9cbab2ee16cf3ac
Rinat Sungatullin <rinat.sun.9952@gmail.com>
2026-07-20 22:01:08 +0400

    ParentId в Department исправлен, может принимать null

--- backend/DirectoryService/src/DirectoryService.Domain/Departments/Department.cs
+++ backend/DirectoryService/src/DirectoryService.Domain/Departments/Department.cs
@@ -46,9 +46,6 @@ public class Department
         !path.EndsWith($"/{slug}", StringComparison.Ordinal))
       throw new InvalidDataException( nameof(path));
     
-    if (parentId == Guid.Empty)
-      throw new InvalidDataException(nameof(parentId));
-    
     this.Id = id;
     
     this.Name = name;
